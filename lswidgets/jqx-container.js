75d83af604b62591c96a12cba24ee4bf62e406ee
Aioub Amini <amini.a@DBI.COM>
2016-09-26 15:48:50 +0330

    RTL Body

--- lswidgets/jqx-container.js
+++ lswidgets/jqx-container.js
@@ -89,7 +89,7 @@
 
 
 
-$(function () {
+$(function() {
     $(".column").sortable({
         connectWith: ".column",
         handle: ".portlet-header",
@@ -103,9 +103,11 @@ $(function () {
         .addClass("ui-widget-header ui-corner-all")
         .prepend("<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
 
-    $(".portlet-toggle").on("click", function () {
+    $(".portlet-toggle").on("click", function() {
         var icon = $(this);
         icon.toggleClass("ui-icon-minusthick ui-icon-plusthick");
         icon.closest(".portlet").find(".portlet-content").toggle();
     });
+
+
 });
